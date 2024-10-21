2695f787748967dd461064d43c18f79c8e0e4b55
Matty Hempstead <matty.hempstead@gmail.com>
2024-10-21 19:49:35 +1100

    Add projects table

--- singleuseapps/app/page.tsx
+++ singleuseapps/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { trpc } from "@/app/api/trpc/trpcClient";
 import { Button } from "@/components/ui/button";
 import { useUserAuth } from "@/hooks/useUserAuth";
 import Editor from "@monaco-editor/react";
@@ -8,6 +9,10 @@ import { useState } from "react";
 export default function Page() {
   const [code, setCode] = useState("// Write your code here");
   const { userAuth, isLoading } = useUserAuth();
+  const { data: currentUser, isLoading: isCurrentUserLoading } =
+    trpc.user.getCurrentUser.useQuery(undefined, {
+      enabled: !!userAuth,
+    });
 
   return (
     <div className="grid grid-cols-2 min-h-screen font-[family-name:var(--font-geist-sans)]">
@@ -23,13 +28,18 @@ export default function Page() {
         <div className="flex flex-col gap-8 items-center sm:items-start">
           test
           <Button>Click me</Button>
-          {isLoading ? (
+          {isLoading || (userAuth && isCurrentUserLoading) ? (
             <p>Loading...</p>
           ) : (
             <>
               <p>User ID: {userAuth?.id}</p>
               {userAuth?.email && !userAuth.app_metadata.provider && (
                 <p>Email: {userAuth.email}</p>
+              )}
+              {currentUser && (
+                <p>
+                  Created At: {new Date(currentUser.createdAt).toLocaleString()}
+                </p>
               )}
             </>
           )}
--- singleuseapps/db/schema.ts
+++ singleuseapps/db/schema.ts
@@ -10,3 +10,20 @@ export const usersTable = pgTable("users", {
 
 export type UsersSelect = typeof usersTable.$inferSelect;
 export type UsersInsert = typeof usersTable.$inferInsert;
+
+export const projectsTable = pgTable("projects", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => usersTable.userId, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true, mode: "string" })
+    .defaultNow()
+    .notNull(),
+  sourceCode: text("source_code").notNull(),
+});
+
+export type ProjectsSelect = typeof projectsTable.$inferSelect;
+export type ProjectsInsert = typeof projectsTable.$inferInsert;
